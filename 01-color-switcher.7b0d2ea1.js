6f8451296ba74ee1d0fd598e330b192da56ce7d6
svet-shaigen <kuzovasvitlana@gmail.com>
2023-03-19 16:36:54 +0000

    Deploying to gh-pages from @ svet-shaigen/goit-js-hw-09@f91fe5455ff2986c42beb38940a6d044d7ac213c 🚀

--- 01-color-switcher.7b0d2ea1.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]");function n(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){document.body.style.backgroundColor=n(),setInterval(n,1e3)})),btnStop.addEventListener("click",onStop)}();
-//# sourceMappingURL=01-color-switcher.7b0d2ea1.js.map
