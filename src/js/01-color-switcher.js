f91fe5455ff2986c42beb38940a6d044d7ac213c
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2023-03-19 18:36:32 +0200

    Update changed files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,29 @@
 const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
 const CHANGE_BODY_COLOR = 1000;
+
 let timerId = null;
 
-btnStart.addEventListener('click', onStart);
+
+btnStart.addEventListener('click', onStart); 
 btnStop.addEventListener('click', onStop);
 
 function onStart() {
-   document.body.style.backgroundColor = getRandomHexColor();
-   
-   timerId = setInterval(getRandomHexColor, 1000);
+  timerId = setInterval(()=> {
+  document.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_BODY_COLOR);
+
+  btnStart.disabled = true;
+  btnStop.disabled = false;
 }
 
+function onStop() {
+  clearInterval(timerId);
 
-
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
 
 
 function getRandomHexColor() {
