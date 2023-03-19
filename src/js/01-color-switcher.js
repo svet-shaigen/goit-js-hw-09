8e24b8a240135f7ae748302637ce592215929e0e
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2023-03-19 16:17:00 +0200

    Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const btnStart = document.querySelector('[data-start]');
+
+const CHANGE_BODY_COLOR = 1000;
+let timerId = null;
+
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
+
+function onStart() {
+   document.body.style.backgroundColor = getRandomHexColor();
+   
+   timerId = setInterval(getRandomHexColor, 1000);
+}
+
+
+
+
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
